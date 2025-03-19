568981387ed4e863619d12527c70f3d2bf3499a6
rukaSUKII <sobonukasz@gmail.com>
2025-03-19 19:20:56 +0100

    Apply translations to registration, change layout of the view, fix logic of registration

--- lib/pages/register_screen.dart
+++ lib/pages/register_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile/l10n/app_localizations.dart';
+import 'package:mobile/main.dart';
+import 'package:mobile/tools/navigators.dart';
+import 'package:mobile/tools/token_handler.dart';
 import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'main_page.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  const RegisterScreen({super.key});
 
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
@@ -56,7 +58,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
         // Sprawdzenie czy hasła są zgodne
         if (_passwordController.text != _confirmPasswordController.text) {
           setState(() {
-            _errorMessage = 'Hasła nie są zgodne';
+            _errorMessage = AppLocalizations.of(
+              context,
+            ).translate("register_password_confirmation_failed");
             _isLoading = false;
           });
           return;
@@ -78,21 +82,36 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
         // Wysłanie żądania rejestracji
         final response = await http.post(
-          Uri.parse('https://twoja-domena.pl/api/register'),
+          Uri.parse('$baseURL/api/auth/register'),
           headers: {'Content-Type': 'application/json'},
           body: jsonEncode(registerData),
         );
-
+        final responseData = jsonDecode(response.body);
         if (response.statusCode == 200) {
           // Sukces - przetwarzanie otrzymanego JWT
-          final responseData = jsonDecode(response.body);
-          final jwtToken = responseData['token'];
           
+          final jwtToken = responseData['accessToken'];
+
           // Zapisanie tokenu JWT
-          await _saveToken(jwtToken);
-          
+          await TokenHandler.saveToken(jwtToken);
+
           // Przejście do głównego ekranu
-          _navigateToHome();
+          Navigators.navigateToHome(context);
+        } else if (response.statusCode == 400) {
+          String warningMessage = "";
+          if (responseData["detail"].toString() == "common_password"){
+            warningMessage = AppLocalizations.of(context).translate("register_common_password");
+          }
+          else{
+              AppLocalizations.of(context,).translate("register_account_in_database");
+          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                warningMessage,
+              ),
+            ),
+          );
         } else {
           // Błąd - wyświetlenie komunikatu
           final errorData = jsonDecode(response.body);
@@ -100,11 +119,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
             _errorMessage = errorData['detail'] ?? 'Błąd rejestracji';
           });
         }
-      } catch (e) {
-        setState(() {
-          _errorMessage = 'Błąd połączenia: $e';
-        });
-      } finally {
+      } 
+      finally {
         setState(() {
           _isLoading = false;
         });
@@ -112,24 +128,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
   }
 
-  // Zapisywanie tokenu JWT
-  Future<void> _saveToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('jwt_token', token);
-  }
-
-  // Nawigacja do ekranu głównego
-  void _navigateToHome() {
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => const HomeScreen()),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rejestracja'),
+        title: Text(AppLocalizations.of(context).translate("register_app_bar")),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -140,213 +143,236 @@ class _RegisterScreenState extends State<RegisterScreen> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const SizedBox(height: 20),
-                
+
                 // Ikona rejestracji
                 const Icon(
                   Icons.app_registration,
                   size: 80,
                   color: Colors.blue,
                 ),
-                
+
                 const SizedBox(height: 20),
-                
+
                 // Sekcja: Dane konta
-                const Text(
-                  'Dane konta',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+                Text(
+                  AppLocalizations.of(context).translate("register_label"),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 10),
-                
+
                 // Pole nazwa użytkownika
                 TextFormField(
                   controller: _usernameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nazwa użytkownika *',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_username"),
                     prefixIcon: Icon(Icons.person),
                     border: OutlineInputBorder(),
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Proszę wprowadzić nazwę użytkownika';
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_username_missing");
                     }
                     return null;
                   },
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole email
                 TextFormField(
                   controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email *',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_email"),
                     prefixIcon: Icon(Icons.email),
                     border: OutlineInputBorder(),
                   ),
                   keyboardType: TextInputType.emailAddress,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Proszę wprowadzić email';
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_email_missing");
                     }
-                    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                        .hasMatch(value)) {
-                      return 'Proszę wprowadzić prawidłowy email';
+                    if (!RegExp(
+                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                    ).hasMatch(value)) {
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_email_wrong_format");
                     }
                     return null;
                   },
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole hasło
                 TextFormField(
                   controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Hasło *',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_password_confirm"),
                     prefixIcon: Icon(Icons.lock),
                     border: OutlineInputBorder(),
                   ),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Proszę wprowadzić hasło';
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_password_missing");
                     }
-                    if (value.length < 6) {
-                      return 'Hasło musi mieć co najmniej 6 znaków';
+                    if (value.length < 12) {
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_ password_too_short");
                     }
                     return null;
                   },
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole potwierdzenie hasła
                 TextFormField(
                   controller: _confirmPasswordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Potwierdź hasło *',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_password_confirm"),
                     prefixIcon: Icon(Icons.lock_outline),
                     border: OutlineInputBorder(),
                   ),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Proszę potwierdzić hasło';
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_password_confirmation_missing");
                     }
                     if (value != _passwordController.text) {
-                      return 'Hasła nie są zgodne';
+                      return AppLocalizations.of(
+                        context,
+                      ).translate("register_password_confirmation_failed");
                     }
                     return null;
                   },
                 ),
-                
+
                 const SizedBox(height: 24),
-                
+
                 // Sekcja: Dane osobowe (opcjonalne)
-                const Text(
-                  'Dane osobowe (opcjonalnie)',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+                Text(
+                  AppLocalizations.of(
+                    context,
+                  ).translate("register_personal_data"),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 10),
-                
+
                 // Pole imię
                 TextFormField(
                   controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Imię',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_first_name"),
                     prefixIcon: Icon(Icons.person_outline),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole nazwisko
                 TextFormField(
                   controller: _surnameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nazwisko',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_last_name"),
                     prefixIcon: Icon(Icons.person_outline),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
-                const SizedBox(height: 24),
-                
-                // Sekcja: Adres (opcjonalny)
-                const Text(
-                  'Adres (opcjonalnie)',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                
+
+                const SizedBox(height: 16),
+
                 // Pole ulica
                 TextFormField(
                   controller: _streetController,
-                  decoration: const InputDecoration(
-                    labelText: 'Ulica',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_street"),
                     prefixIcon: Icon(Icons.location_on_outlined),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole nr budynku
                 TextFormField(
                   controller: _buildingController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nr budynku/mieszkania',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_building_no"),
                     prefixIcon: Icon(Icons.home_outlined),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole miasto
                 TextFormField(
                   controller: _cityController,
-                  decoration: const InputDecoration(
-                    labelText: 'Miasto',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_city"),
                     prefixIcon: Icon(Icons.location_city_outlined),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole kod pocztowy
                 TextFormField(
                   controller: _postalCodeController,
-                  decoration: const InputDecoration(
-                    labelText: 'Kod pocztowy',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_postal_code"),
                     prefixIcon: Icon(Icons.markunread_mailbox_outlined),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Pole kraj
                 TextFormField(
                   controller: _countryController,
-                  decoration: const InputDecoration(
-                    labelText: 'Kraj',
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(
+                      context,
+                    ).translate("register_country"),
                     prefixIcon: Icon(Icons.public),
                     border: OutlineInputBorder(),
                   ),
                 ),
-                
+
                 // Komunikat błędu
                 if (_errorMessage != null)
                   Padding(
@@ -359,34 +385,33 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       ),
                     ),
                   ),
-                
+
                 const SizedBox(height: 32),
-                
-                // Przycisk rejestracji
-                ElevatedButton(
+                FilledButton(
                   onPressed: _isLoading ? null : _register,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: _isLoading
-                      ? const CircularProgressIndicator()
-                      : const Text(
-                          'Zarejestruj się',
-                          style: TextStyle(fontSize: 16),
-                        ),
+                  child:
+                      _isLoading
+                          ? const CircularProgressIndicator()
+                          : Text(
+                            AppLocalizations.of(
+                              context,
+                            ).translate("register_link"),
+                            style: TextStyle(fontSize: 16),
+                          ),
                 ),
-                
+
                 const SizedBox(height: 16),
-                
+
                 // Powrót do logowania
                 TextButton(
                   onPressed: () => Navigator.pop(context),
-                  child: const Text('Masz już konto? Zaloguj się'),
+                  child: Text(
+                    AppLocalizations.of(
+                      context,
+                    ).translate("register_account_exist"),
+                  ),
                 ),
-                
+
                 const SizedBox(height: 24),
               ],
             ),
