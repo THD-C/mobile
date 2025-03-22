3a713eb260e606639f4f7da07de2dba6d3c88327
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-03-22 16:37:32 +0100

    Add config class

---
+++ lib/config/app_config.dart
@@ -0,0 +1,21 @@
+class AppConfig {
+  static final AppConfig _instance = AppConfig._internal();
+
+  factory AppConfig() {
+    return _instance;
+  }
+
+  AppConfig._internal();
+
+  String _apiBaseUrl = 'http://thdc';
+
+  String get fiatApiUrl => '$_apiBaseUrl/api/currency';
+
+  String get cryptoApiUrl => '$_apiBaseUrl/api/crypto';
+
+  void initialize({String? apiBaseUrl}) {
+    if (apiBaseUrl != null) {
+      _apiBaseUrl = apiBaseUrl;
+    }
+  }
+}
